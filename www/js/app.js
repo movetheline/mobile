f113b755a2d915bf84af78fb089fa224268eb7f8
Andy Corman <andycorm@gmail.com>
2016-09-22 10:59:08 -0700

    Tweaks to app.js file

--- www/js/app.js
+++ www/js/app.js
@@ -10,19 +10,18 @@ var app = {
     //DOMAIN_URL: 'http://192.168.0.22/',
     //BROWSER_OPTIONS: null,
 
-    AUTH_LOGIN_ACTION_URL: 'index.php?action=Auth/login&utm_source=dash_mobile&utm_medium=mobile_app&utm_campaign=mobile_app',
-
     // These are never used to redirect the browser, only to detect if we're on the respective pages
     FACEBOOK_LOGIN_URL_FLAG: 'https://m.facebook.com/',
 
     lastURL: null, // Used for bouncing back after an external link has been accessed
 
     startApplication: function(){
-        app.openNewDashWindow(app.DOMAIN_URL + 'dash/' + app.AUTH_LOGIN_ACTION_URL);
+        app.openNewDashWindow(app.DOMAIN_URL);
     },
 
     openNewDashWindow: function(url){
         app.browserWindow = window.open(url, '_blank', app.BROWSER_OPTIONS);
+        app.fetchContacts();
         app.browserWindow.addEventListener('loadstart', app.onLoadStart, false);
         app.browserWindow.addEventListener('loadstop', app.onLoadStop, false);
         app.browserWindow.addEventListener('exit', function(){
@@ -37,7 +36,7 @@ var app = {
             navigator.notification.activityStart("Loading", "Please Wait...");
         }
 
-        app.browserWindow.executeScript({code: "window.dispatchEvent(new Event('mobileAppOnLoadStart2'));"});
+        app.browserWindow.executeScript({code: "window.dispatchEvent(new Event('mobileAppOnLoadStart'));"});
     },
 
     onLoadStop: function(event){
@@ -52,7 +51,33 @@ var app = {
             return;
         }
         app.lastURL = event.url; // Record the last URL in case we open an external URL and have to backup
-        app.browserWindow.executeScript({code: "window.dispatchEvent(new Event('mobileAppOnLoadStop2'));"});
+        app.browserWindow.executeScript({code: "window.dispatchEvent(new Event('mobileAppOnLoadStop'));"});
+        app.setContactsInPage();
+    },
+
+    fetchContacts: function() {
+
+        // find all contacts with 'Bob' in any name field
+        var options = new ContactFindOptions();
+        options.filter = '';
+        options.multiple = true;
+        navigator.contacts.find(['*'], app.contactFetchSuccess, app.contactFetchFail, options);
+    },
+
+    // onSuccess: Get a snapshot of the current contacts
+    contactFetchSuccess: function(contacts) {
+        console.log(contacts);
+        app.contacts = contacts;
+        app.setContactsInPage();
+    },
+
+    contactFetchFail: function(contactError) {
+        console.log(contactError);
+        alert('onError!');
+    },
+
+    setContactsInPage: function() {
+        app.browserWindow.executeScript({code: "sessionStorage.setItem('contacts', JSON.stringify(" + JSON.stringify(app.contacts) + "));"});
     },
 
     /**
